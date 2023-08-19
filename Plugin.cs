e496625df84ef5f0a4ea34dfe833798e7723ff66
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-08-19 17:07:42 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,73 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace Deaths_Reach_Returns
+{
+    [BepInPlugin("com.meds.deathsreachreturns", "Death's Reach Returns", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new("com.meds.deathsreachreturns");
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            Log = Logger;
+            // Plugin startup logic
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch]
+    public class ComeBackPls
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Globals), "CreateGameContent")]
+        public static void CreateGameContentPostfix()
+        {
+            string path = "file:///" + Path.Combine(BepInEx.Paths.PluginPath, "Fear1.wav").Replace("\\", "/");
+            //Plugin.Log.LogDebug("PATH: " + path);
+            try
+            {
+                using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(path, AudioType.WAV))
+                {
+                    www.SendWebRequest();
+                    while (!www.isDone)
+                    {
+
+                    }
+
+                    if (www.result == UnityWebRequest.Result.ConnectionError)
+                    {
+                        Plugin.Log.LogDebug(www.error);
+                    }
+                    else
+                    {
+                        Dictionary<string, CardData> medsCardsSource = Traverse.Create(Globals.Instance).Field("_CardsSource").GetValue<Dictionary<string, CardData>>();
+                        Dictionary<string, CardData> medsCards = Traverse.Create(Globals.Instance).Field("_Cards").GetValue<Dictionary<string, CardData>>();
+                        AudioClip ac = DownloadHandlerAudioClip.GetContent(www);
+                        ac.name = "Fear1";
+                        medsCardsSource["deathsreach"].Sound = ac;
+                        medsCards["deathsreach"].Sound = ac;
+                        medsCardsSource["deathsreacha"].Sound = ac;
+                        medsCards["deathsreacha"].Sound = ac;
+                        medsCardsSource["deathsreachb"].Sound = ac;
+                        medsCards["deathsreachb"].Sound = ac;
+                        medsCardsSource["deathsreachrare"].Sound = ac;
+                        medsCards["deathsreachrare"].Sound = ac;
+                        Traverse.Create(Globals.Instance).Field("_CardsSource").SetValue(medsCardsSource);
+                        Traverse.Create(Globals.Instance).Field("_Cards").SetValue(medsCards);
+                        Plugin.Log.LogInfo($"FTONG! The original Death's Reach audio has returned!");
+                    }
+                }
+            }
+            catch { }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Death's Reach Returns
+
+Do you miss the loving thwomp that was the old Death's Reach sound? This mod brings it back.
+
+## Installation
+
+Install [BepInEx](https://across-the-obelisk.thunderstore.io/package/BepInEx/BepInExPack_AcrossTheObelisk/).
+
+Download the latest release and put it in your BepInEx _plugins_ folder.
+
+## Support
+
+Ping @stiffmeds in the #modding channel of the [official Across the Obelisk Discord](https://discord.gg/across-the-obelisk-679706811108163701).
